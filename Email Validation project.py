f79c73b9bb9fd90d1eeb38c3c30de08ea1d4b7fd
Ashwini Kumar <110678563+Ak9525@users.noreply.github.com>
2023-09-27 13:26:37 +0530

    Add files via upload

---
+++ Email Validation project.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install nbconvert
+
+
+# In[2]:
+
+
+# CONDITIONS
+
+#a-z
+#0-9
+#. _ ,time 1
+# @ time 1
+# . ,from back 2nd and 3rd 
+
+
+# In[3]:
+
+
+# import re
+
+
+# In[4]:
+
+
+# email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+
+
+# In[5]:
+
+
+# user_email=input('Enter your Email ')
+
+
+# In[6]:
+
+
+# if re.search(email_condition,user_email):
+    # print("Right Email")
+# else:
+    # print("Wrong Email")
+
+
+# In[ ]:
+
+
+
+
